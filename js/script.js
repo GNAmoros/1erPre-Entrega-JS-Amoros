e4ce09e1c79125c292d35ba0e286e9feca8850bb
Nicolas Amoros <nicolas.amoros@gmail.com>
2023-06-02 23:33:48 -0300

    comienzo proyecto

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function computerPlay() {
+  const choices = ['rock', 'paper', 'scissors'];
+}
+
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection();
+
+  if () {   } 
+  else if () {  } 
+  else {  }
+}

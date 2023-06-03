fa3b647964de77cd2afbcbf78b6166715df19486
Nicolas Amoros <nicolas.amoros@gmail.com>
2023-06-03 18:38:23 -0300

    working on score

--- js/script.js
+++ js/script.js
@@ -16,3 +16,22 @@ function playRound(playerSelection, computerSelection) {
     return 'Perdiste'
     }
 }
+
+function game () {
+  let playerScore = 0;
+  let computerScore = 0;
+  let rounds = 3;
+
+  for (let i = 0; i < rounds; i++) {
+    const playerSelection = prompt('Elige: piedra, papel o tijera');
+    const computerSelection = computerPlay();
+    const result = playRound(playerSelection, computerSelection);
+    console.log(result);
+
+    if (result.includes('Ganaste')) {
+      playerScore++;
+    } else if (result.includes('Perdiste')) {
+      computerScore++;
+    }
+  }
+}
